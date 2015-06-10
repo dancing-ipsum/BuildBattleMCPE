0fbeaad00872beec1a6310df88beec3e6c59ddf0
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-06-10 17:07:49 +0400

    Create lugin.php

---
+++ src/BuildBattleMCPE/lugin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MCPEGamerJP\BuildBattleMCPE;
+
+use pocketmine\player\playerdata;
+use pocketmine\tiles\item\Diamond_Sword;
+use pocketmine\plugin\pluginbase
+
+class BuildBattleMCPE extends PluginBase(){
+ 
