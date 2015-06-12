ac0db1ec2263de9713d2814c545048fe6ae10ee2
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-06-12 10:32:17 +0400

    Update plugin.php

--- src/BuildBattleMCPE/plugin.php
+++ src/BuildBattleMCPE/plugin.php
@@ -10,4 +10,22 @@ use pocketmine\player\playerjoinevent;
 class BuildBattleMCPE extends PluginBase(){
   public function sendMessage($player,$message){
      $player->sendMessage("§3Welcome§4to§5a§6Build§1Battle §8Server!")
+  }
+  
+  public function transferPlayer(Player $player, $address, $port = 19132, $message = "You are joining a BuildBattle game, please wait."){
+    $ev = new PlayerTransferEvent($player, $address, $port, $message);
+    $this->getServer()->getPluginManager()->callEvent($ev);
+    if($ev->isCancelled()){
+      return false;
+    }
+   
+    $ip = $this->lookupAddress($ev->getAddress());
+    
+    if($ip === null){
+      return false;
+    }
+    
+    if($message !== null and $message !== ""){
+      //Stopping code here for today xD
+  }
  
