9e3c4e07f01c523af498a217beb3b7256d9c8391
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-12-02 12:05:06 +0400

    Update Main.php

--- src/MCPEGamerJPatGitHub/BuildBattleMCPE/Main.php
+++ src/MCPEGamerJPatGitHub/BuildBattleMCPE/Main.php
@@ -13,14 +13,17 @@ class Main extends PluginBase(){
      $player->sendMessage("§3Welcome§4to§5a§6Build§1Battle §8Server!")
   }
   
-  public function transferPlayer($player, $destination)
+  public function transferPlayer(){
      $player->sendMessage("You are being transferred to a BuildIt game!")
      $player->transferPlayer("BuildBattleLobby")
      $sender->getLogger(".$player. joined a BuildBattles game!")
   }
   
-  public function priorGame($player, $message)
-     $player->sendPopup("Waiting for game start...")
- }
+  public function priorGame(){
+     $player->sendPopup("Welcome " .$name "to the BuildBattles lobby!" };
+     
  
   public function lobbyWait(
+    
+costant;
+
